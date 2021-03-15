0e86c57deaa2cb6f1c20cc05a5d3ec435d7c1639
agostilu <agostilu@sheridancollege.ca>
2021-03-15 17:45:24 -0400

    Added my PC to the Connection String and made sure the DB was created

--- Startup.cs
+++ Startup.cs
@@ -28,14 +28,16 @@ namespace LloydStephanieRealty
             services.AddRazorPages();
             services.AddDbContext<MBS_DBContext>(opts =>
             {
-                opts.UseSqlServer(Configuration["ConnectionStrings:MBSConnStr"]);
+                opts.UseSqlServer(Configuration["ConnectionStrings:LuigiConnection"]);
             });
             services.AddScoped<IUserRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MBS_DBContext context)
         {
+            context.Database.EnsureCreated();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
